fdf6b63c3467d115d3c53511bd1fb12757866ff8
Allyssa Kate Maranan <59193566+allymaranan@users.noreply.github.com>
2022-07-29 03:15:07 +0800

    added files

---
+++ login-client/login-client.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-client',
+  templateUrl: './login-client.page.html',
+  styleUrls: ['./login-client.page.scss'],
+})
+export class LoginClientPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
